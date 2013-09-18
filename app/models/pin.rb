991d117a33fcc229f4e6cbe383708d1820e5230e
Christina Cox <christina.elleni.cox@gmail.com>
2013-09-18 10:58:40 -0700

    Add Paperclip Gem

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,10 +3,9 @@ class Pin < ActiveRecord::Base
 
   validates :description, presence: true
   validates :user_id, presence: true
+  
+  belongs_to :user
   validates_attachment :image, presence: true,
                             content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
                             size: { less_than: 5.megabytes }
-  belongs_to :user
-  validates_attachment :image, styles: { medium: "320x240>"}
-
 end
