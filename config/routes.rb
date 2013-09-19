3f21fe37264b787bafbc396eb6f9e839e1b3ef89
Christina Cox <christina.elleni.cox@gmail.com>
2013-09-19 09:46:52 -0700

    pin.rb changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Omrails::Application.routes.draw do
 
   devise_for :users
 
-  match 'users/:id' => 'users#show', as: :user
+  get "users/show"
 
+  match 'users/:id' => 'users#show', as: :user
+ 
  get 'about' => 'pages#about'
 
  root :to => 'pages#home'
