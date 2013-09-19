cba0570ecc345d77fc19ee3bae313c1bbc64f6d1
Christina Cox <christina.elleni.cox@gmail.com>
2013-09-19 09:06:20 -0700

    pin.rb changes

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -5,10 +5,10 @@ class Pin < ActiveRecord::Base
   validates :user_id, presence: true
 
   belongs_to :user
-  has_attached_file :image, styles: { medium: "320x240" }
+	has_attached_file :image, styles: { medium: "320x240" }
 	validates_attachment :image, presence: true,
-                        content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
-                        size: { less_than: 5.megabytes }
+												content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
+												size: { less_than: 5.megabytes }
   
   
 end
